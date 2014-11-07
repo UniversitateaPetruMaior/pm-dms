21003831907eba8c862a8508c08dfecb7763ca09
Cosmin Marian <test@test.test>
2014-11-07 22:23:18 +0200

    small updates after running on the real server: removed unused i18n SQL s, renamed extra SQL, renamed sign tool executable

--- OpenKM/project_resources/db_scripts/extra_en-GB.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO OKM_TRANSLATION (TR_MODULE, TR_KEY, TR_TEXT, TR_LANGUAGE) VALUES ('frontend', 'document.signatures.applied', 'Signed by', 'en-GB');
-INSERT INTO OKM_TRANSLATION (TR_MODULE, TR_KEY, TR_TEXT, TR_LANGUAGE) VALUES ('frontend', 'general.menu.sign.tool', 'Signing Tool', 'en-GB');
---
+++ OpenKM/project_resources/db_scripts/extra_i18n.sql
@@ -0,0 +1,4 @@
+INSERT INTO OKM_TRANSLATION (TR_MODULE, TR_KEY, TR_TEXT, TR_LANGUAGE) VALUES ('frontend', 'document.signatures.applied', 'Signed by', 'en-GB');
+INSERT INTO OKM_TRANSLATION (TR_MODULE, TR_KEY, TR_TEXT, TR_LANGUAGE) VALUES ('frontend', 'general.menu.sign.tool', 'Signing Tool', 'en-GB');
+INSERT INTO OKM_TRANSLATION (TR_MODULE, TR_KEY, TR_TEXT, TR_LANGUAGE) VALUES ('frontend', 'document.signatures.applied', 'Firmado por', 'es-ES');
+INSERT INTO OKM_TRANSLATION (TR_MODULE, TR_KEY, TR_TEXT, TR_LANGUAGE) VALUES ('frontend', 'general.menu.sign.tool', 'Firma utensilio', 'es-ES');
